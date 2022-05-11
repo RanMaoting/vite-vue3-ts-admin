b7f7870035059c8caf47a01e05e5910065b96eab
dream <1012377328@qq.com>
2022-05-11 20:34:46 +0800

    添加配置,集成unocss

--- build/plugins.ts
+++ build/plugins.ts
@@ -4,12 +4,17 @@
 
 import { PluginOption } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import Unocss from 'unocss/vite';
+import { presetAttributify, presetUno } from 'unocss';
 
 export default function getPluginsList(): PluginOption[] {
     return [
         vue({
             // 开启是实验性质的响应性语法糖
             reactivityTransform: true,
+        }),
+        Unocss({
+            presets: [presetAttributify(), presetUno()],
         }),
     ];
 }
