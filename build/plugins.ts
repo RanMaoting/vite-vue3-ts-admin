253d0f40fd9fe58b3055081bf53b2f999855aa60
冉茂廷 <1012377328@qq.com>
2022-05-11 11:49:52 +0800

    first commit

---
+++ build/plugins.ts
@@ -0,0 +1,15 @@
+/**
+ * @file vite配置列表
+ */
+
+import { PluginOption } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+export default function getPluginsList(): PluginOption[] {
+    return [
+        vue({
+            // 开启是实验性质的响应性语法糖
+            reactivityTransform: true,
+        }),
+    ];
+}
---
+++ vite.config.ts
@@ -0,0 +1,33 @@
+import { resolve } from "path";
+import { defineConfig, UserConfig, ServerOptions } from "vite";
+import getPluginsList from "./build/plugins";
+
+// 使用node转换路径
+const pathResolve = (dir: string): string => {
+    return resolve(__dirname, ".", dir);
+};
+
+// 路径别名
+const alias: Record<string, string> = {
+    "/@/": pathResolve("src"),
+    "/#/": pathResolve("build"),
+};
+// 开发服务器
+const server: ServerOptions = {
+    host: true,
+    port: 4396,
+};
+
+// https://vitejs.dev/config/
+export default defineConfig((): UserConfig => {
+    return {
+        resolve: {
+            // 别名
+            alias,
+        },
+        // 插件列表
+        plugins: getPluginsList(),
+        // 开发服务器配置
+        server,
+    };
+});
