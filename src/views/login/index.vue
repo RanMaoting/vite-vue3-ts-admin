66c6eb3a1ebc063b5a6f7da09de3a5dd10efcd7d
冉茂廷 <1012377328@qq.com>
2022-05-14 15:44:58 +0800

    update

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,17 +1,89 @@
 <template>
-    <div class="w-screen h-screen flex justify-center items-center">
-        <!-- <div class="w-50% h-50%">asdasd</div> -->
-        <div>
-            <NButton type="primary"> button </NButton>
+    <div
+        class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8 mt-20vh"
+    >
+        <div class="max-w-md w-full space-y-8">
+            <div>
+                <img
+                    class="mx-auto h-12 w-auto"
+                    src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                    alt="Workflow"
+                />
+                <h2
+                    class="mt-6 text-center text-3xl font-extrabold text-gray-900"
+                >
+                    Sign in to your account
+                </h2>
+            </div>
+            <form class="mt-8 space-y-6" action="#" method="POST">
+                <input type="hidden" name="remember" value="true" />
+                <div class="rounded-md shadow-sm -space-y-px">
+                    <div>
+                        <label for="email-address" class="sr-only">账号</label>
+                        <input
+                            id="email-address"
+                            v-model="username"
+                            name="email"
+                            autocomplete="email"
+                            class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                            placeholder="账号"
+                        />
+                    </div>
+                    <div>
+                        <label for="password" class="sr-only">密码</label>
+                        <input
+                            id="password"
+                            v-model="password"
+                            name="password"
+                            type="password"
+                            autocomplete="current-password"
+                            class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                            placeholder="密码"
+                        />
+                    </div>
+                </div>
+
+                <div class="flex items-center justify-between">
+                    <div class="flex items-center">
+                        <input
+                            id="remember-me"
+                            name="remember-me"
+                            type="checkbox"
+                            class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+                        />
+                        <label
+                            for="remember-me"
+                            class="ml-2 block text-sm text-gray-900"
+                        >
+                            Remember me
+                        </label>
+                    </div>
+
+                    <div class="text-sm">
+                        <a
+                            href="#"
+                            class="font-medium text-indigo-600 hover:text-indigo-500"
+                        >
+                            Forgot your password?
+                        </a>
+                    </div>
+                </div>
+
+                <div>
+                    <n-button type="primary" class="!w-full" @click="login">
+                        Sign in
+                    </n-button>
+                </div>
+            </form>
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-
-const a = ref(11);
-a.value = 345;
+// import { LockClosedIcon } from '@heroicons/vue/solid';
+const username = $ref<string>('');
+const password = $ref<string>('');
+const login = () => {
+    console.log(username, password);
+};
 </script>
-
-<style scoped></style>
