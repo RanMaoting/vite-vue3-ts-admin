cc941704093f5b5d7b06e2c271c2bcd5cbddcd6a
dream <1012377328@qq.com>
2022-05-25 21:36:10 +0800

    update

--- build/vite/plugins.ts
+++ build/vite/plugins.ts
@@ -1,3 +1,13 @@
+/*
+ * @Author: dream 1012377328@qq.com
+ * @Date: 2022-05-25 20:19:54
+ * @LastEditors: dream 1012377328@qq.com
+ * @LastEditTime: 2022-05-25 21:35:13
+ * @FilePath: \vite-vue3-ts-admin\build\vite\plugins.ts
+ * @Description:
+ *
+ * Copyright (c) 2022 by dream 1012377328@qq.com, All Rights Reserved.
+ */
 /**
  * @file vite配置列表
  */
@@ -7,11 +17,12 @@ import vue from '@vitejs/plugin-vue';
 import Unocss from 'unocss/vite';
 
 export default function getPluginsList(): PluginOption[] {
-    return [
+    const pluginList = [
         vue({
             // 开启是实验性质的响应性语法糖
             reactivityTransform: true,
         }),
         Unocss(),
     ];
+    return pluginList;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,25 @@
+/*
+ * @Author: dream 1012377328@qq.com
+ * @Date: 2022-05-25 20:19:54
+ * @LastEditors: dream 1012377328@qq.com
+ * @LastEditTime: 2022-05-25 20:39:00
+ * @FilePath: \vite-vue3-ts-admin\src\main.ts
+ * @Description:
+ *
+ * Copyright (c) 2022 by dream 1012377328@qq.com, All Rights Reserved.
+ */
 import { createApp } from 'vue';
 
-import 'uno.css';
 import '@unocss/reset/tailwind.css';
+import 'uno.css';
 
 import App from './App.vue';
 import store from './store';
 import router from './router';
+
+const meta = document.createElement('meta');
+meta.name = 'naive-ui-style';
+document.head.appendChild(meta);
 
 const app = createApp(App);
 
